efb56a2d8800a7be8076c47e839a89188d71a9f4
seyedmahdiamin <seyedmahdiamin1998@gmail.com>
2023-03-25 14:41:40 +0430

    Debug

--- ETL.py
+++ ETL.py
@@ -21,24 +21,33 @@ DL.connection()
 products = DL.engine.execute('''select * from products''')
 
 for product in products:
-
+    print('\n'*3)
     product = dict(zip(products.keys(), product))
 
     specifications = DL.engine.execute(f'''SELECT * FROM specifications
                                            WHERE "lotId" = {product['lotId']}''')
 
-    specification_keys = [  'Size', 'Gender', 'Total_Weight',\
+    specification_keys = [  'Size', 'Gender', 'Total Weight',\
                             'Type', 'Material', 'Condition',\
-                            'Laboratory_Report', 'Material_Fineness',\
-                            'Main_Stone','Brand']
+                            'Laboratory Report', 'Material Fineness',\
+                            'Main Stone','Brand']
     
     specification_list = {}
     for specification in specifications:
         result_dict = dict(zip(specifications.keys(), specification ))
-        product[result_dict['name']] = result_dict['value'] 
+        if result_dict['name']:
+            product[result_dict['name'].replace(' ', '_')] = result_dict['value'] 
+
 
     for item in specification_keys:
+        item = item.replace(' ', '_')
         if item not in product.keys():
             product[item] = None  
+    
+    if product['Total_Weight']:
+        product['Total_Weight'] = product['Total_Weight'].replace(' g','')
+    
+    if product['Material_Fineness']:
+        product['Material_Fineness'] = product['Material_Fineness'].replace(' kt.','')
     
     DW.store_product(table=product_DW, item=product)
